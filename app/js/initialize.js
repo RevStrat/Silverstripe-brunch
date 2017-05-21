85d8222f95aad49d82d8d81a560254bc585cb836
John Robert Rankin <ithic@me.com>
2017-05-21 17:09:23 -0300

    added initialization require, restructured to separate styles from js

--- app/js/initialize.js
+++ app/js/initialize.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
-  
   require('slick-carousel');
+
+  //console.info($('body'));
 });
